56b2986b3f414bb769aa1d0caaa8371165596500
Aibek <nomad.aibek@gmail.com>
2020-07-31 20:08:19 +0600

    Added validators to amount and price

--- source/webapp/migrations/0001_initial.py
+++ source/webapp/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, verbose_name='Название')),
                 ('description', models.CharField(blank=True, max_length=2000, null=True, verbose_name='Описание')),
-                ('category', models.CharField(choices=[('other', 'Разное'), ('food', 'Еда'), ('tech', 'Бытовая техника'), ('tools', 'Интсрументы'), ('toys', 'Игрушки')], default='other', max_length=20, verbose_name='Категории')),
+                ('category', models.CharField(choices=[('other', 'Разное'), ('food', 'Еда'), ('tech', 'Бытовая техника'), ('tools', 'Интсрументы'), ('toys', 'Игрушки')], default='other', max_length=20, verbose_name='Категория')),
                 ('amount', models.IntegerField(verbose_name='Остаток')),
                 ('price', models.DecimalField(decimal_places=2, max_digits=7, verbose_name='Цена')),
             ],
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 DEFAULT_CATEGORY = 'other'
@@ -14,9 +15,11 @@ CATEGORY_CHOICES = (
 class Product(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Название')
     description = models.CharField(max_length=2000, null=True, blank=True, verbose_name='Описание')
-    category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES, verbose_name='Категории')
-    amount = models.IntegerField(verbose_name='Остаток')
-    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2)
+    category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES,
+                                verbose_name='Категория')
+    amount = models.IntegerField(verbose_name='Остаток', validators=(MinValueValidator(0),))
+    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2,
+                                validators=(MinValueValidator(0),))
 
     def __str__(self):
         return f'{self.name} - {self.amount}'
